83e7b1cc868f9c9942a913f59f88d37f27f0dc22
Aryan Patel <ajpatel44@myseneca.ca>
2025-05-22 00:29:07 -0400

    Added labs

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Aryan Patel
+# Author ID: ajpatel44
+# Date Created: 2025/05/22
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
